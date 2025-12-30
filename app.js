17f634a6a3782ef8f27f21bd91623cbc52042122
Humza <humzaanwarkhan@gmail.com>
2025-12-31 00:10:10 +0530

    Update API URL for production

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/auth", authRoute);
